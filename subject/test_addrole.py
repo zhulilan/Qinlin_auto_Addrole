931aaa3849842b1d13542c653eeb1b787293aa2a
zhull <919405689@qq.com>
2021-10-26 18:10:24 +0800

    提交代码

--- subject/test_addrole.py
+++ subject/test_addrole.py
@@ -2,10 +2,9 @@
 # @Author   : zhulilan
 # @FILE     : test_addrole.py
 # @Time     : 2021/10/22 16:10
-from selenium import webdriver
 from time import sleep
 
-# from selenium.webdriver import ActionChains
+from selenium import webdriver
 
 
 class TestAddrole():
@@ -15,7 +14,7 @@ class TestAddrole():
         self.driver.implicitly_wait(5)
 
     def teardown(self):
-        #self.driver.quit()
+        # self.driver.quit()
         pass
     def test_role(self):
         self.driver.get("https://precrm.joyobpo.com")
@@ -25,38 +24,24 @@ class TestAddrole():
 
         #点击基础设置
         self.driver.find_element_by_xpath("//a[contains(@href,'/basicsetting/basicinfo')]").click()
-        sleep(3)
+        sleep(2)
         #点击权限管理
         self.driver.find_elements_by_class_name("el-submenu")[1].click()
         sleep(2)
         #点击角色管理
         self.driver.find_element_by_xpath("//a[contains(@href,'/basicsetting/role-management')]").click()
-        sleep(3)
+        sleep(2)
         #点击新增角色按钮
         self.driver.find_element_by_css_selector("button.jy-button.table-operation-item.jy-button--primary").click()
-        sleep(3)
+        sleep(2)
         #输入角色名称
         self.driver.find_elements_by_class_name("el-input__inner")[1].send_keys("zll自动化新增角色")
-        sleep(3)
-        # for i in checkboxs:
-        #     # checkboxs= self.driver.find_elements_by_class_name('el-checkbox__inner')
-        #     # ActionChains(self.driver).move_to_element('el-checkbox__inner').perform()
-        #     i.click()
-            # sleep(3)
-
-
+        sleep(2)
         # checkboxs=self.driver.find_element_by_css_selector('input[type=checkbox]')
         # for checkbox in checkboxs:
         #     checkbox.click()
         #     sleep(2)
-        # checkboxs = self.driver.find_elements_by_class_name('el-checkbox__input')
-        # if checkboxs:
-        #     for check in checkboxs:
-        #         check.click()
-        #         sleep(2)
-        # else:
-        #     print("没有找到元素")
-        #勾选菜单
+        #勾选所有菜单
         self.driver.find_element_by_css_selector(".el-table__row:nth-child(1) .el-checkbox__inner").click()
         self.driver.find_element_by_css_selector(".el-table__row:nth-child(3) .el-checkbox__inner").click()
         self.driver.find_element_by_css_selector(".el-table__row:nth-child(5) .el-checkbox__inner").click()
@@ -77,10 +62,13 @@ class TestAddrole():
         self.driver.find_element_by_css_selector( ".el-table__row:nth-child(388) .el-checkbox__inner").click()
         self.driver.find_element_by_css_selector( ".el-table__row:nth-child(399) .el-checkbox__inner").click()
         self.driver.find_element_by_css_selector(".el-table__row:nth-child(423) .el-checkbox__inner").click()
+        #点击下一步
         self.driver.find_element_by_css_selector(".btn_right.btn.button_primary").click()
         sleep(2)
+        #菜单权限设置全部权限
         for i in range(1,335):
-            ar=str(i)
-            self.driver.find_element_by_css_selector(".el-table__row:nth-child("+ar+") .codeclick:nth-child(1)").click()
-
+            x=str(i)
+            self.driver.find_element_by_css_selector(".el-table__row:nth-child("+x+") .codeclick:nth-child(1)").click()
+        #点击保存
+        self.driver.find_element_by_css_selector(".btn_right.btn.button_primary").click()
 
